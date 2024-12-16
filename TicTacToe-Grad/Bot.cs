7ca1dfe02a7c395ccbdcb630c287e13d044d7337
TorLindstrom <tor.lindstrom@winlas.se>
2024-12-16 14:41:52 +0100

    feat: scripted game runnable from terminal

--- TicTacToe-Grad/Bot.cs
+++ TicTacToe-Grad/Bot.cs
@@ -6,6 +6,9 @@ public class Bot(string marker)
 
 	public virtual void PlayMove(Board any)
 	{
-		
+		any.GetAvailableTiles()
+			.Where(tile => tile.X == 0)
+			.ToList()
+			.ForEach(tile => tile.Mark(Marker));
 	}
 }
